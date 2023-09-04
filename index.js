9932e23732d706a47acbc0f22e26184a6ee65f42
autismo245 <70080158+autismo245@users.noreply.github.com>
2023-09-04 17:08:03 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-$(".btn").on("click", function() {
-    var strain = ["red congo", "lemon OG", "super lemon haze", "strawberry cough", "chiquita banana", "king louis", "pink god"];
-    var random = Math.floor(Math.random() * 7) + 1;
-    var strainPick = strain[random]
-    $(".strain").text(strainPick)
-})
